cb05bc22ae0bfaea2a7d1a1f7891198f28946edd
hukangkang <hukangkang@xiaomi.com>
2023-11-29 16:51:35 +0800

    tree构建修改

--- common/src/test/java/TreeSolution.java
+++ common/src/test/java/TreeSolution.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -7,6 +6,45 @@ import java.util.List;
 import java.util.Set;
 
 public class TreeSolution {
+
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.right == null && root.left == null) {
+            return true;
+        } else if (root.right == null || root.left == null) {
+            return false;
+        }
+        Deque<TreeNode> leftQueue = new LinkedList<>();
+        Deque<TreeNode> rightQueue = new LinkedList<>();
+        leftQueue.add(root.left);
+        rightQueue.add(root.right);
+        while (!leftQueue.isEmpty() && !rightQueue.isEmpty()) {
+            TreeNode first = leftQueue.removeFirst();
+            TreeNode last = rightQueue.removeLast();
+            if (first.val == last.val) {
+                if (first.left != null) {
+                    leftQueue.addFirst(first.left);
+                }
+                if (last.right != null) {
+                    rightQueue.addLast(last.right);
+                }
+
+                if (last.left != null) {
+                    leftQueue.addFirst(last.left);
+                }
+                if (first.right != null) {
+                    rightQueue.addLast(first.right);
+                }
+            } else {
+                return false;
+            }
+        }
+        return leftQueue.isEmpty() && rightQueue.isEmpty();
+    }
+
 
     public List<Integer> inorderTraversal(TreeNode root) {
         if (root == null) {
@@ -35,9 +73,11 @@ public class TreeSolution {
     public static void main(String[] args) {
         TreeSolution solution = new TreeSolution();
         TreeNode root = buildTreeNode(new Integer[]{
-            1, null, 2, 3
+            // 1, 2, 2, null, 3, null, 3
+            1, 2, 2, 3, 4, 4, 3
         });
-        System.out.println(Arrays.toString(solution.inorderTraversal(root).toArray()));
+        // System.out.println(Arrays.toString(solution.inorderTraversal(root).toArray()));
+        System.out.println(solution.isSymmetric(root));
     }
 
     private static TreeNode buildTreeNode(Integer[] nodes) {
@@ -52,15 +92,15 @@ public class TreeSolution {
             if (treeNodes[i] == null) {
                 treeNodes[i] = new TreeNode(nodes[i]);
             }
-            int left = i + 1;
-            int right = i + 2;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
             if (left < nodes.length && nodes[left] != null) {
-                TreeNode leftNode = new TreeNode(left);
+                TreeNode leftNode = new TreeNode(nodes[left]);
                 treeNodes[i].left = leftNode;
                 treeNodes[left] = leftNode;
             }
             if (right < nodes.length && nodes[right] != null) {
-                TreeNode rightNode = new TreeNode(right);
+                TreeNode rightNode = new TreeNode(nodes[right]);
                 treeNodes[i].right = rightNode;
                 treeNodes[right] = rightNode;
             }
