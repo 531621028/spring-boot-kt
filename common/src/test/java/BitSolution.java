74f22b20ec8a078c5eeb7d7ae1df6d89f69274bd
hukangkang <hukangkang@xiaomi.com>
2023-12-04 17:20:47 +0800

    比特位计数

--- common/src/test/java/BitSolution.java
+++ common/src/test/java/BitSolution.java
@@ -1,4 +1,33 @@
+import java.util.Arrays;
+
 public class BitSolution {
+
+    public static void main(String[] args) {
+        BitSolution bitSolution = new BitSolution();
+        int[] nums = new int[]{
+            // 7, 1, 5, 3, 6, 4
+            4, 1, 2, 1, 2
+        };
+        System.out.println(bitSolution.singleNumber(nums));
+        System.out.println(Arrays.toString(bitSolution.countBits(2)));
+        System.out.println(Arrays.toString(bitSolution.countBits(5)));
+    }
+
+    /**
+     * 比特为计数， 对于任意整数 x，令 x=x & (x−1)，该运算将 x 的二进制表示的最后一个 1 变成 0
+     */
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int cnt = 0, j = i;
+            while (j != 0) {
+                cnt++;
+                j = j & (j - 1);
+            }
+            result[i] = cnt;
+        }
+        return result;
+    }
 
     /**
      * 异或运算有一个性质：任何数和自己做异或运算的结果为 0。同时，任何数和 0 做异或运算的结果都是它本身。
@@ -9,14 +38,5 @@ public class BitSolution {
             result = result ^ num;
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        BitSolution bitSolution = new BitSolution();
-        int[] nums = new int[]{
-            // 7, 1, 5, 3, 6, 4
-            4, 1, 2, 1, 2
-        };
-        System.out.println(bitSolution.singleNumber(nums));
     }
 }
