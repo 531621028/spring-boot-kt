ba7f067759b9dfdd98fa39d4fd50b896b559caa6
hukangkang <hukangkang@xiaomi.com>
2023-12-11 11:13:15 +0800

    电话号码的组合

--- common/src/test/java/medium/StringSolution.java
+++ common/src/test/java/medium/StringSolution.java
@@ -1,18 +1,72 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class StringSolution {
 
     public static void main(String[] args) {
         StringSolution solution = new StringSolution();
-        System.out.println(solution.longestPalindrome("babad"));
-        System.out.println(solution.longestPalindrome("cbbd"));
-        // System.out.println(solution.longestPalindrome("pwwkew"));
-        // System.out.println(solution.longestPalindrome("ccaabb"));
-
+        // System.out.println(solution.longestPalindrome("babad"));
+        // System.out.println(solution.longestPalindrome("cbbd"));
+        // // System.out.println(solution.longestPalindrome("pwwkew"));
+        // // System.out.println(solution.longestPalindrome("ccaabb"));
+        // System.out.println(solution.longestPalindrome("babad"));
+        System.out.println(Arrays.toString(solution.letterCombinations("234").toArray()));
     }
+
+    /**
+     * 电话号码的组合
+     */
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() < 1) {
+            return Collections.emptyList();
+        }
+        Map<String, List<String>> phoneNumber = phoneNumber();
+        List<String> result = phoneNumber.get(String.valueOf(digits.charAt(0)));
+        for (int i = 1; i < digits.length(); i++) {
+            List<String> start = new ArrayList<>(result);
+            result = new ArrayList<>();
+            String number = String.valueOf(digits.charAt(i));
+            List<String> chars = phoneNumber.getOrDefault(number, Collections.emptyList());
+            for (String s : start) {
+                result.addAll(composeNextChar(chars, s));
+            }
+        }
+        return result;
+    }
+
+    private List<String> composeNextChar(List<String> list, String nextChar) {
+        if (list.isEmpty()) {
+            return Collections.singletonList(nextChar);
+        }
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(nextChar + s);
+        }
+        return result;
+    }
+
+
+    private Map<String, List<String>> phoneNumber() {
+        Map<String, List<String>> phoneNumber = new HashMap<>();
+        phoneNumber.put("2", Arrays.asList("a", "b", "c"));
+        phoneNumber.put("3", Arrays.asList("d", "e", "f"));
+        phoneNumber.put("4", Arrays.asList("g", "h", "i"));
+        phoneNumber.put("5", Arrays.asList("j", "k", "l"));
+        phoneNumber.put("6", Arrays.asList("m", "n", "o"));
+        phoneNumber.put("7", Arrays.asList("p", "q", "r", "s"));
+        phoneNumber.put("8", Arrays.asList("t", "u", "v"));
+        phoneNumber.put("9", Arrays.asList("w", "x", "y", "z"));
+        return phoneNumber;
+    }
+
 
     /**
      * 最长回文子串
