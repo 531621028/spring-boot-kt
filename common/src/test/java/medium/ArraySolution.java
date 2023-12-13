984e4057a1df0b5f8f9363de36e1739809945cdd
hukangkang <hukangkang@xiaomi.com>
2023-12-13 15:10:30 +0800

    下一个排列

--- common/src/test/java/medium/ArraySolution.java
+++ common/src/test/java/medium/ArraySolution.java
@@ -9,18 +9,58 @@ public class ArraySolution {
 
     public static void main(String[] args) {
         ArraySolution solution = new ArraySolution();
-        int[] arrays = new int[]{
-            // -1, 0, 1, 2, -1, -4
-            // 3,-2,1,0
-            // -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4
-            // -2, 0, 0, 2, 2
-            34, 55, 79, 28, 46, 33, 2, 48, 31, -3, 84, 71, 52, -3, 93, 15, 21, -43, 57, -6, 86, 56, 94, 74, 83, -14, 28, -66, 46, -49, 62,
-            -11, 43, 65, 77, 12, 47, 61, 26, 1, 13, 29, 55, -82, 76, 26, 15, -29, 36, -29, 10, -70, 69, 17, 49
-        };
+        // int[] arrays = new int[]{
+        // -1, 0, 1, 2, -1, -4
+        // 3,-2,1,0
+        // -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4
+        // -2, 0, 0, 2, 2
+        //     34, 55, 79, 28, 46, 33, 2, 48, 31, -3, 84, 71, 52, -3, 93, 15, 21, -43, 57, -6, 86, 56, 94, 74, 83, -14, 28, -66, 46, -49, 62,
+        //     -11, 43, 65, 77, 12, 47, 61, 26, 1, 13, 29, 55, -82, 76, 26, 15, -29, 36, -29, 10, -70, 69, 17, 49
+        // };
         // System.out.println(solution.maxArea(arrays));
-        System.out.println(Arrays.toString(solution.threeSum(arrays).toArray()));
+        // System.out.println(Arrays.toString(solution.threeSum(arrays).toArray()));
+        int[] nums = new int[]{
+            // 1, 3, 2
+            4, 2, 0, 2, 3, 2, 0
+        };
+        solution.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
+
+    /**
+     * 下一个排列
+     */
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 1) {
+            return;
+        }
+        int minIndex = -1;
+        int minGrater = Integer.MAX_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    if (nums[j] < minGrater) {
+                        minGrater = nums[j];
+                        minIndex = j;
+                    }
+                }
+            }
+            if (minIndex > 0) {
+                swap(nums, i, minIndex);
+                Arrays.sort(nums, i + 1, nums.length);
+                return;
+            }
+
+        }
+        Arrays.sort(nums);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
 
     /**
      * 三数之和
