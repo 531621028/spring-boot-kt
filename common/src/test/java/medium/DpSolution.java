a4217bab91ab7d64015393a91ed8ee0b370a27ca
hukangkang <hukangkang@xiaomi.com>
2023-12-25 17:01:18 +0800

    编辑距离

--- common/src/test/java/medium/DpSolution.java
+++ common/src/test/java/medium/DpSolution.java
@@ -9,10 +9,41 @@ public class DpSolution {
         //     2, 3, 1, 1, 4
         // };
         // System.out.println(solution.canJump(prices));
-        int[][] grid = new int[][]{
-            {1, 3, 1}, {1, 5, 1}, {4, 2, 1}
-        };
-        System.out.println(solution.minPathSum(grid));
+        // int[][] grid = new int[][]{
+        //     {1, 3, 1}, {1, 5, 1}, {4, 2, 1}
+        // };
+        // System.out.println(solution.minPathSum(grid));
+        System.out.println(solution.minDistance("horse", "ros"));
+        System.out.println(solution.minDistance("intention", "execution"));
+        System.out.println(solution.minDistance("b", ""));
+    }
+
+
+    /**
+     * 编辑距离
+     */
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] + 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = dp[0][i - 1] + 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int min = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    min = Math.min(min, dp[i - 1][j - 1]);
+                    dp[i][j] = min + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     public int minPathSum(int[][] grid) {
